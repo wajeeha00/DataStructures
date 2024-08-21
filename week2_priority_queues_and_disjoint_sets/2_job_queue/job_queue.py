e9ebb007332bd8922c65adad88764f19661d3e13
wajeeha00 <wajeehaaftab7890@gmail.com>
2024-08-21 11:44:24 +0500

    Update job_queue.py

--- week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
+++ week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
@@ -1,32 +1,31 @@
-# python3
-
 from collections import namedtuple
-
+import heapq
+ 
 AssignedJob = namedtuple("AssignedJob", ["worker", "started_at"])
-
-
+ 
+ 
 def assign_jobs(n_workers, jobs):
     # TODO: replace this code with a faster algorithm.
+    heap = [(0, i) for i in range(n_workers)]
+    heapq.heapify(heap)
     result = []
-    next_free_time = [0] * n_workers
     for job in jobs:
-        next_worker = min(range(n_workers), key=lambda w: next_free_time[w])
-        result.append(AssignedJob(next_worker, next_free_time[next_worker]))
-        next_free_time[next_worker] += job
-
+        next_free_time, worker = heapq.heappop(heap)
+        result.append(AssignedJob(worker, next_free_time))
+        heapq.heappush(heap, (next_free_time+job, worker))
     return result
-
-
+ 
+ 
 def main():
     n_workers, n_jobs = map(int, input().split())
     jobs = list(map(int, input().split()))
     assert len(jobs) == n_jobs
-
+ 
     assigned_jobs = assign_jobs(n_workers, jobs)
-
+ 
     for job in assigned_jobs:
         print(job.worker, job.started_at)
-
-
+ 
+ 
 if __name__ == "__main__":
     main()
